866cab90f6c66407cd7339312e2bc0e1471ed1bd
RossPin <dorpetz@hotmail.com>
2018-12-10 16:50:07 +1300

    set config vars from options, check for special options

--- user_upload.php
+++ user_upload.php
@@ -1,8 +1,17 @@
 #!/usr/bin/php
 <?php
-$options = getopt ("u:p:h:", ["file:", "create_table", "dry_run", "help"]);
-$file = $options['file'];
-$host = $options['h'];
-$username = $options['u'];
-$password = $options['p'];
+$options = getopt ('u:p:h:', ['file:', 'create_table', 'dry_run', 'help']);
+$file = $options['file'] ?? null;
+$host = $options['h'] ?? null;
+$username = $options['u'] ?? null;
+$password = $options['p'] ?? null;
+if (array_key_exists('help',$options)) {
+  die("help\n");
+}
+if (array_key_exists('dry_run',$options)) {
+  die("dry run\n");
+}
+if (array_key_exists('create_table',$options)) {
+  die("create table\n");
+}
 ?>
