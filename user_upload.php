da59b85a434e3d330bc5a8eb062af9f9ca208899
RossPin <dorpetz@hotmail.com>
2018-12-10 17:07:20 +1300

    add function to display help

--- user_upload.php
+++ user_upload.php
@@ -5,13 +5,21 @@ $file = $options['file'] ?? null;
 $host = $options['h'] ?? null;
 $username = $options['u'] ?? null;
 $password = $options['p'] ?? null;
+$dry_run = array_key_exists('dry_run',$options);
 if (array_key_exists('help',$options)) {
-  die("help\n");
-}
-if (array_key_exists('dry_run',$options)) {
-  die("dry run\n");
-}
-if (array_key_exists('create_table',$options)) {
+  help();
+}elseif (array_key_exists('create_table',$options)) {
   die("create table\n");
+}
+
+function help(){
+  echo "user_upload.php can be used with the following options\n\n";
+  echo "• --file [csv file name] – this is the name of the CSV to be parsed\n";
+  echo "• --create_table – this will cause the MySQL users table to be built (and no further action will be taken)\n";
+  echo "• --dry_run – this will be used with the --file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered\n";
+  echo "• -u – MySQL username\n";
+  echo "• -p – MySQL password\n";
+  echo "• -h – MySQL host\n";
+  die("\n");
 }
 ?>
